410cc60b043579fd02fa0ccf0f5c7988104ff79e
Steffen Herrdum <steffen.herrdum@uni-ulm.de>
2014-01-13 16:16:05 +0100

    continued with server implementation

--- src/de/uniulm/bagception/calendar/service/CalendarService.java
+++ src/de/uniulm/bagception/calendar/service/CalendarService.java
@@ -1,8 +1,13 @@
 package de.uniulm.bagception.calendar.service;
 
 
+import java.util.HashSet;
+
 import android.app.IntentService;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.ResultReceiver;
 import android.util.Log;
@@ -10,6 +15,7 @@ import android.util.Log;
 public class CalendarService extends IntentService {
 
 	private ResultReceiver resultReceiver;
+	private Cursor cursor;
 
 	public CalendarService() {
 		super("CalendarService");
@@ -19,6 +25,21 @@ public class CalendarService extends IntentService {
 	protected void onHandleIntent(Intent intent) {
 		resultReceiver = intent.getParcelableExtra("receiverTag");
 		log("request received!");
+		
+		String[] calendarNames = null;
+		int[] calendarIDs = null;
+		
+		if(intent.hasExtra("calendarNames")){
+			calendarNames = intent.getStringArrayExtra("calendarNames");
+		}
+		if(intent.hasExtra("calendarIDs")){
+			calendarIDs = intent.getIntArrayExtra("calendarIDs");
+		}
+	
+		CalendarReader reader = new CalendarReader();
+		reader.readCalendarEvent(this, calendarNames, calendarIDs);
+		
+		
 		Bundle b = new Bundle();
 		b.putString("payload", "itworks");
 		resultReceiver.send(0, b);

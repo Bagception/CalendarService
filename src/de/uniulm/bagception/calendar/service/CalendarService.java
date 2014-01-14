2a88c2686bb82ed7413c2facc251daaed03124da
Steffen Herrdum <steffen.herrdum@uni-ulm.de>
2014-01-14 16:42:10 +0100

    added calendar name resolver

--- src/de/uniulm/bagception/calendar/service/CalendarService.java
+++ src/de/uniulm/bagception/calendar/service/CalendarService.java
@@ -12,37 +12,44 @@ import android.util.Log;
 public class CalendarService extends IntentService {
 
 	private ResultReceiver resultReceiver;
-
+	private CalendarReader reader;
+	
 	public CalendarService() {
 		super("CalendarService");
+		reader = new CalendarReader();
+
 	}
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
 		resultReceiver = intent.getParcelableExtra("receiverTag");
-		log("request received!");
+		log("calendar request received!");
 		
-		String[] calendarNames = null;
-		int[] calendarIDs = null;
 		
-		if(intent.hasExtra("calendarNames")){
-			calendarNames = intent.getStringArrayExtra("calendarNames");
+		if(intent.hasExtra("requestType")){
+			if(intent.getStringExtra("requestType").equals("calendarEvents")){
+				// getting intent extras
+				String[] calendarNames = intent.getStringArrayExtra("calendarNames");
+				int[] calendarIDs = intent.getIntArrayExtra("calendarIDs");
+				int numberOfEvents = intent.getIntExtra("numberOfEvents", -1);
+				
+				// getting and sending calendar events
+				Bundle b = new Bundle();
+				b.putStringArrayList("calendarEvents", reader.readCalendarEvent(this, calendarNames, calendarIDs, numberOfEvents));
+				resultReceiver.send(0, b);
+				log("sending calendar events...");
+			}
+			if(intent.getStringExtra("requestType").equals("calendarNames")){
+				Bundle b = new Bundle();
+				b.putStringArrayList("calendarNames", reader.getCalendarNames(this));
+				resultReceiver.send(0, b);
+				log("sending calendar names...");
+			}
 		}
-		if(intent.hasExtra("calendarIDs")){
-			calendarIDs = intent.getIntArrayExtra("calendarIDs");
-		}
-	
-		CalendarReader reader = new CalendarReader();
-		ArrayList<String> calendarEvents = reader.readCalendarEvent(this, calendarNames, calendarIDs);
 		
-		Bundle b = new Bundle();
-		
-		b.putStringArrayList("calendarEvents", calendarEvents);
-		resultReceiver.send(0, b);
-		log("sending answer...");
 	}
 
 	private void log(String s){
-		Log.d("Service", s);
+		Log.d("CalendarService", s);
 	}
 }
